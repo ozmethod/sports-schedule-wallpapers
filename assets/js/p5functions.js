a2af4651375aed67bc5829d43fda34e58ce9d168
ozmethod <ozmethod_n@hotmail.com>
2026-02-01 14:34:09 -0600

    Comment out offsetY adjustment for February
    Commented out the adjustment for offsetY in February to prevent overlapping issues.

--- assets/js/p5functions.js
+++ assets/js/p5functions.js
@@ -288,7 +288,7 @@ const sketch = (p) => {
         // in that specific scenario to avoid overlapping.
         if (date.getMonth() === 1 && p.daysInMonth(date.getMonth(), date.getFullYear()) === 28 && dayOfWeek_firstDay === 0) {
             offsetY += (WallpaperData.dateBlock.height + WallpaperData.dateBlock.offset.y);
-            //offsetY += (WallpaperData.dateBlock.height / 2);
+            ////offsetY += (WallpaperData.dateBlock.height / 2);
             // Recompute timezone offset anchor after moving the box down
             //offsetY_TimeZone = offsetY + WallpaperData.month.home_away.size.height / 2;
             offsetY_TimeZone = offsetY + WallpaperData.month.home_away.size.height;
